7541f1632e214c81fac7d29cd67684aae37dffdf
Rolark <vova171002@gmail.com>
2020-11-15 16:30:47 +0300

    1.0

---
+++ Scripts/ArrowHolder.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class ArrowHolder : MonoBehaviour
+{
+    public float BPM; // скорость движения в ударах в минуту
+    public bool playable = false; // движутся плитки в данный момент или нет
+    public KeyCode startButton; // кнопка начала движения
+    public KeyCode pauseButton;
+
+    private bool pauseToggle = false;
+    void Start()
+    {
+        BPM = BPM / 60f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playable)
+        {
+            if (Input.GetKeyDown(startButton)) playable = true; // начинаем движение при нажатии старт кнопки
+        }
+        else
+        {
+            transform.position = transform.position - new Vector3(0f, BPM * Time.deltaTime, 0f); // приращиваем к У координате смещение вниз
+        }
+        if (Input.GetKeyDown(pauseButton)) pauseChange();
+    }
+    public void pauseChange()
+    {
+        // установка "паузы" 
+        pauseToggle = !pauseToggle;
+        if (pauseToggle)
+        {
+            playable = false;
+            transform.position += new Vector3(0f, BPM , 0f); //отодвигаем плитки повыше "как в пианисте"
+        }
+        else
+        {
+            playable = true;
+        }
+    }
+}
---
+++ Scripts/ButtonController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour {
+
+    private SpriteRenderer theSR;
+    //public Sprite defaultImage;
+    //public Sprite pressedImage;
+
+    public KeyCode keyToPress;
+    public Color defaultColor = new Color(1f, 1f, 1f);
+    public Color pressedColor = new Color(0.7f, 0.7f, 0.7f, 0.7f);
+    public bool pressed; //нажата кнопка сейчас или нет (хотя ля, можно же клавишу отслеживать...)
+    public bool blocked;
+    void Start()
+    {
+        
+        theSR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPress)) {
+            theSR.color = pressedColor; //немного "анимации" для нажимающихся кнопОчек
+            pressed = true;
+        }
+        if (Input.GetKeyUp(keyToPress)) {
+            theSR.color = defaultColor;
+            pressed = false;
+        }
+        
+    }
+    private void OnTriggerEnter2D(Collider2D note)
+    {
+        if (pressed)//если во время "входа" плашки кнопка нажата
+        {
+            blocked = true;//то она блокируется то ее отжатия и повторного нажатия
+        }//крч не выйдет просто зажать все кнопки и выиграть
+        
+        
+    }
+    private void OnTriggerStay2D(Collider2D note)
+    {
+        if (!pressed && blocked)
+        { 
+            blocked = false; //снятие блокировки
+        }
+        if (!blocked && pressed)
+        {
+            note.gameObject.SetActive(false);//удаление ноты (будет более подробная обработка нажатия)
+        }
+    }
+}
